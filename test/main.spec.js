44a196521db08f0efc8e740b33b9cc9a4cb67af1
zkan <kan@prontomarketing.com>
2015-08-01 13:41:29 +0700

    contacts should be an array

--- test/main.spec.js
+++ test/main.spec.js
@@ -5,4 +5,11 @@ describe("ContactService", function() {
             expect(ContactService.contacts).not.toBeUndefined();
         });
     });
+
+    it("should have contacts as an array", function() {
+        module("Contactical");
+        inject(function(ContactService) {
+            expect(ContactService.contacts).toEqual(jasmine.any(Array))
+        });
+    });
 });
