9e4ff84aeadc9bd3843106a4296acdcb1bb80e03
zkan <kan@prontomarketing.com>
2015-08-01 10:29:14 +0700

    try fizzbuzz using jasmine

---
+++ app/fizzbuzz.js
@@ -0,0 +1,14 @@
+fizzbuzz = function(input) {
+    if(input % 3 == 0 && input % 5 == 0) {
+        return "fizzbuzz";
+    }
+    else if(input % 3 == 0) {
+        return "fizz";
+    }
+    else if (input % 5 == 0) {
+        return "buzz";
+    }
+    else {
+        return input;
+    }
+};
